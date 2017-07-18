73179423e0bfe137a732bde27ef971b92331889e
DenisVS <denvsim@gmail.com>
2017-07-18 07:46:27 +0000

    Initial release hide_links

---
+++ files/lang/English/hidden_links.php
@@ -0,0 +1,9 @@
+<?php
+
+// Language definitions used by the hiddenlinks
+$lang_hiddenlinks = array(
+
+'Register to view page'		    		=>	'Sorry, but you do not have permission to view this Page. Please register or login.',
+'Link'		    		=>	'Link',
+
+);
---
+++ files/lang/Russian/hidden_links.php
@@ -0,0 +1,10 @@
+﻿<?php
+ 
+// Language definitions used by the hiddenlinks
+$lang_hiddenlinks = array(
+ 
+'Register to view page'		    		=>	'Для перехода по ссылке Вы должны войти или зарегистрироваться.',
+'Link'		    		=>	'Ссылка',
+ 
+);
+
