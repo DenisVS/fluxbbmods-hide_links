58c4d6687924ba01abcb14926876cc1a3677c4d5
DenisVS <denvsim@gmail.com>
2017-07-18 13:00:28 +0000

    translate fixed

--- files/lang/English/hidden_links.php
+++ files/lang/English/hidden_links.php
@@ -3,7 +3,7 @@
 // Language definitions used by the hiddenlinks
 $lang_hiddenlinks = array(
 
-'Register to view page'		    		=>	'Sorry, but you do not have permission to view this Page. Please register or login.',
+'Register to view page'		    		=>	'Sorry, but you do not have permission to view this page. Please register or login.',
 'Link'		    		=>	'Link',
 
 );
